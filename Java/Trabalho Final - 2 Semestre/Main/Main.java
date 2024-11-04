2650b20923a838b1b7b2a010e259b4177a0b6d64
Carlos3628 <carlosalexandrekadu@gmail.com>
2024-11-04 15:04:00 -0300

    Delete Java/Trabalho Final - 2 Semestre/Main directory

--- Java/Trabalho Final - 2 Semestre/Main/Main.java
+++
@@ -1,5 +0,0 @@
-import java.util.Random;
-
-public class Main {
-    Random gerador = new Random();
-}
--- Java/Trabalho Final - 2 Semestre/Main/Matricula.java
+++
@@ -1,7 +0,0 @@
-public class Matricula {
-    private int numMatricula;
-    
-    public void Matricula(int numMatricula){
-        this.numMatricula = numMatricula;
-    }
-}
